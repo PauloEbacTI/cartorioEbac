4d397aeb06ff68ba02756cab0eb6a37968368715
PauloEbacTI <122576131+PauloEbacTI@users.noreply.github.com>
2023-01-12 19:59:07 -0300

    Atualizacao para o modulo 4

--- cartorio.c
+++ cartorio.c
@@ -4,6 +4,99 @@
 #include <string.h>// biblioteca responsável por cuidar das strings
 
 
+int registro() // Função responsável por cadastrar os usuários no sistema
+{
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: \n");
+	scanf("%s", &cpf);
+	
+	strcpy(arquivo, cpf); // responsavel por copiar os valores das strings
+	
+	FILE *file; // cria o arquivo
+	
+	file = fopen(arquivo, "w"); // cria o arquivo
+	fprintf(file, cpf); // salva o valor da variavel
+	fclose(file); // fecha o arquivo
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: \n");
+	scanf("%s", &nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: \n");
+	scanf("%s", &sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: \n");
+	scanf("%s", &cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file, cargo);
+	fclose(file);
+	
+	system("pause");
+	
+}
+
+int consulta()
+{
+	
+	setlocale(LC_ALL, "Portuguese");
+	char cpf [40];
+	char conteudo[200];
+	 
+	printf("Digite o cpf a ser consultado: \n");
+	scanf("%s", cpf);
+	printf("\n");
+	 
+	FILE *file;
+	file = fopen(cpf, "r");
+	 
+	if(file == NULL)
+	{
+		printf("Não foi possível achar o arquivo. Não localizado. \n");
+	}
+	
+	while(fgets(conteudo, 200, file)!= NULL)
+	{
+		printf("Essas São as informações do usuário: \n");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	fclose(file);
+	system("pause");
+	 
+	 
+}
+
+int deletar()
+{
+	printf("Você escolheu a opção de deletar um cadastro.\n\n");
+	system("pause");
+}
+
 int main()
 {
 	int opcao = 0; // Definindo as variáveis
@@ -20,7 +113,6 @@ int main()
 		printf("\t1- Registrar nomes.\n");
 		printf("\t2- Consultar nomes.\n");
 		printf("\t3- Deletar nomes.\n");
-		printf("\t4- Sair do sistema.\n\n");
 		printf("Opção: "); // Fim do menu
 		
 		scanf("%d", &opcao); // Armazenando a escolha do usuário
@@ -30,23 +122,20 @@ int main()
 		switch(opcao)
 		{
 			case 1:
-			printf("- Você escolheu registrar alunos.\n");
-			system("pause");
+				registro();
 			break;
 			
 			case 2:
-			printf("- Você escolheu consultar os alunos.\n");
-			system("pause");
+				consulta();
 			break;	
 			
 			case 3:
-			printf("- Você escolheu deletar um cadastro.\n");
-			system("pause");
+				deletar();
 			break;
-							
+			
 			default:
-			printf("Essa opção não esta disponível!\n\n");
-			system("pause");
+				printf("Essa opção não esta disponível!\n\n");
+				getch();
 			break;	
 				
 		}
